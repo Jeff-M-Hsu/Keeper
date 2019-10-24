bbd463b0bec8c7788233a6b655ea23e22aa93f18
Jeff-M-Hsu <jeff_m_hsu@hotmail.com>
2019-10-24 15:50:24 -0700

    Updated environment to integrate jhsu-components

--- keeper/src/components/Main/HomePage/index.js
+++ keeper/src/components/Main/HomePage/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Tile from './Tile';
+import { Button } from 'jhsu-components';
 
 class HomePage extends Component{
   render(){
     return(
       <div id="home page" className="nav-page slid-in">
+            <Button theme="primary" size="medium">Hello</Button>
             <div className="h-16"/>
             <section className="tile-row">
               <Tile />
