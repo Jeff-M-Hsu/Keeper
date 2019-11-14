0b8b3a9a26119cf99038a18864b8dbfca34045c4
Jeff-M-Hsu <jeff_m_hsu@hotmail.com>
2019-11-13 22:03:57 -0800

    Added JHsu Components to test

--- keeper/src/components/Main/HomePage/index.js
+++ keeper/src/components/Main/HomePage/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Tile from './Tile';
-import { Button } from 'jhsu-components';
+import { Button, Menu, MenuItem, Header, Icon, Card, Accordion } from 'jhsu-components';
 
 class HomePage extends Component{
+  handleClick = () => {
+    alert("hello");
+  };
   render(){
     return(
       <div id="home page" className="nav-page slid-in">
-            <Button theme="primary" size="medium">Hello</Button>
+        <Accordion size="large" title="Test Section">
+          <Button theme="primary" size="medium" onClick={this.handleClick}>Hello</Button>
+          <Card size="large"><Icon shape="circle" src="https://dummyimage.com/250/888/fff" /><p><Header>John Smith</Header><br/>Email: John@Smith.com<br/>Phone: 999-999-9999</p></Card>
+          <Menu>
+            <MenuItem href="https://www.npmjs.com/package/jhsu-components" target="__blank" size="medium">Menu Item</MenuItem>
+            <MenuItem size="medium">Menu Item</MenuItem>
+            <MenuItem size="medium">Menu Item</MenuItem>
+          </Menu>
+        </Accordion>
             <div className="h-16"/>
             <section className="tile-row">
               <Tile />
